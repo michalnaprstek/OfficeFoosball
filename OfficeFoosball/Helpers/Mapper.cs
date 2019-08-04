74a42ea7dcc57173a1e5face0f5ac076ebd87722
Michal Náprstek <c_Michal.Naprstek@viasat.com>
2019-08-04 11:46:20 +0200

    after marge fix

--- OfficeFoosball/Helpers/Mapper.cs
+++ OfficeFoosball/Helpers/Mapper.cs
@@ -13,9 +13,9 @@ namespace OfficeFoosball.Helpers
         public static DAL.Entities.Player Map(Models.Player player)
             => new DAL.Entities.Player(player.Id, player.Name);
         public static DAL.Entities.Team Map(Models.Team team)
-            => new DAL.Entities.Team(team.Id, team.Name, team.Player1Id, team.Player2Id);
-        public static Models.Team Map(DAL.Entities.Team team)
-            => new Models.Team(team.Id, team.TeamName, team.Player1, team.Player2);
+            => new DAL.Entities.Team(team.Id, team.Name, team.Player1.Id, team.Player2.Id);
+        public static Models.Team Map(DAL.Entities.Team team, IEnumerable<DAL.Entities.Player> players)
+            => new Models.Team(team.Id, team.TeamName, Map(players.Single(p => p.Id == team.Player1)), Map(players.Single(p => p.Id == team.Player2)));
         public static Models.Match Map(DAL.Entities.Match match)
             => new Models.Match(match.Id, match.TeamYellow, match.TeamRed, match.TeamYellowScore, match.TeamRedScore, match.Note);
         public static DAL.Entities.Match Map(Models.Match match)
