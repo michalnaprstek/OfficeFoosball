78d3a69d5b61ba0f4c87c735b34cd57f570e9350
Michal Náprstek <c_Michal.Naprstek@viasat.com>
2019-12-16 19:14:05 +0100

    statistics style update

--- OfficeFoosball/ClientApp/src/components/player-statistics/PlayerStatistics.js
+++ OfficeFoosball/ClientApp/src/components/player-statistics/PlayerStatistics.js
@@ -7,11 +7,11 @@ export default function PlayerStatistics(props) {
     return (
         stats.map((s, index) => {
             return (
-                <div className="stats-item row" key={index}>
-                    <div className="stats-item__position"><span>{index+1}</span></div>
-                    <div className="row stats-item__container">
-                        <div className="stats-item__percentage">{s.successPercentage.toFixed(1)}%</div>
+                <div className="stats-item" key={index}>
+                    <div className="row">
+                        <div className="stats-item__position"><span>{index+1}</span></div>
                         <div className="stats-item__name">{s.player.name}</div>
+                        <div className="stats-item__percentage">{s.successPercentage.toFixed(1)}%</div>
                     </div>
                 </div>
             );
