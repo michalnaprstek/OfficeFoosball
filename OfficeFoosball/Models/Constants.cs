45591154cce9ca8c00f9d1ad2f2ad1cfe7df3c82
Michal Náprstek <c_Michal.Naprstek@viasat.com>
2020-01-04 16:56:30 +0100

    role names to upper case

--- OfficeFoosball/Models/Constants.cs
+++ OfficeFoosball/Models/Constants.cs
@@ -4,8 +4,7 @@
     {
         public static class UserRoles
         {
-            public const string ADMIN = "admin";
-            public const string BASIC = "basic";
+            public const string ADMIN = "ADMIN";
         }
     }
 }
