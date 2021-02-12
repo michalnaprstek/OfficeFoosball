5ac3cfeef1a05a9c855cf69cd0af93e630036043
Michal Náprstek <michal.naprstek@valatron.com>
2021-02-12 20:03:58 +0100

    style updated, auth improvements

--- OfficeFoosball/ClientApp/src/utils/auth/auth.js
+++ OfficeFoosball/ClientApp/src/utils/auth/auth.js
@@ -1,10 +1,11 @@
-import axiosInstance from "../axiosInstance";
+import axiosInstance from '../axiosInstance';
 
 export class RefreshTokenResult {
   success;
   accessToken;
-  constructor(success, accessToken){
-    if(success && !accessToken){
+
+  constructor(success, accessToken) {
+    if (success && !accessToken) {
       throw new Error('Refresh token result require token for success state.');
     }
     this.success = success;
@@ -24,46 +25,52 @@ export default class Auth {
   };
 
   register = async (username, email, password) => {
-      return await axiosInstance.post('auth/register', {
+    try {
+      await axiosInstance.post('auth/register', {
         username,
         email,
         password
-      })
-      .then(_ => { return { ok: true } })
-      .catch(e => { return { ok:false, errorMessage: e }});
-  }
+      });
+      return { ok: true };
+    } catch (e) {
+      if(e.response && e.response.data) {
+        return { ok: false, errorMessage: e.response.data }
+      }
+      return { ok: false, errorMessage: e };
+    }
+  };
 
   isAuth = () => {
     const accessToken = localStorage.getItem('access_token');
     return accessToken ? true : false;
-  }
+  };
 
   logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
     localStorage.clear();
-  }
+  };
 
   refreshToken = async () => {
     const refreshResponse = await axiosInstance.post('auth/token', {
       'refreshToken': localStorage.getItem('refresh_token')
     });
 
-    if(refreshResponse.status === 201){
+    if (refreshResponse.status === 201) {
       const { data } = refreshResponse;
-      this.storeTokens(data.accessToken, data.refreshToken)
+      this.storeTokens(data.accessToken, data.refreshToken);
       return new RefreshTokenResult(true, data.accessToken);
     }
 
     return new RefreshTokenResult(false, undefined);
-  }
+  };
 
   getToken = () => {
     return localStorage.getItem('access_token');
-  }
+  };
 
   storeTokens(accessToken, refreshToken) {
-    localStorage.setItem("access_token", accessToken);
-    localStorage.setItem("refresh_token", refreshToken);
+    localStorage.setItem('access_token', accessToken);
+    localStorage.setItem('refresh_token', refreshToken);
   }
 }
